ada9f4b320e1cab52c545dbd13e9fdbe410b72d4
kollabhargavre1 <kollabhargavreddy@gmail.com>
2023-01-12 17:20:49 +0530

    Commit0

--- app/photodb/photodb/settings.py
+++ app/photodb/photodb/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@a2svdx4xo3j#7zey%n@t+hl2sg1*$zgzl()dbtud3--2$=*n-'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
